6eb5d758b3cfe4740344cb9b5e824024c819cdb0
Bechera <66968588+Pepsiman715@users.noreply.github.com>
2022-08-01 11:13:44 -0400

    done

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import Head from 'next/head'
-import Meta from '../components/Meta'
-import linkedin from './assets/linkedin.png'
+import React from 'react';
+import Head from 'next/head';
+import Meta from '../components/Meta';
+import linkedin from './assets/linkedin.png';
+import stylesAbout from '../styles/About.module.css';
 export default function about() {
   return (
     <div>
       <Meta title="About"/>
       <h1>About Page</h1>
-      <p>My first Next.js project</p>
       <h3>Things I learned</h3>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>Server-Side-Rendering</li>
+        <li>Nested Routing</li>
+        <li>React Fragments</li>
+        <li>Extracting values from the url</li>
+        <li>How to link between different pages</li>
+        <li>Head tags</li>
+        <li>Layout and CSS modules</li>
+        <li>Static Paths</li>
+        <li>Static Props</li>
+        <li>Fallbacks</li>
+        <li>Meta Tags</li>
       </ul>
+      
+    <div style={{'display': 'flex', 'justifyContent': 'center', 'gap' : '0.5em'}}>
+      <a href  ="https://www.becheraportfolio.ca/" style={{'color': '#0070f3;'}}>Portfolio</a>
+      <br/>
+      <a href ="https://github.com/becheract" style={{'color': '#0070f3;'}}>Github</a>
+      <br/>
+      <a href ="https://www.linkedin.com/in/bechera-chapman-tremblay-49b22b199/" style={{'color': '#0070f3;'}}>LinkedIn</a>
+    </div>
+
     </div>
   )
 }
